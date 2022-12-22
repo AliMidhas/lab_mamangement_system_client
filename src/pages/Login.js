dc80020a4ab1c42c57c9215610792d1918c276d4
Ali Midhas <alimidhas199@gmail.com>
2022-12-23 00:28:55 +0300

    add unsafe url header

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -37,6 +37,7 @@ const Login = () => {
     axios
       .post(
         "https://labmanagementsystemapi-production.up.railway.app/api/auth/login",
+        { referrerPolicy: "unsafe_url" },
         credentials
       )
       .then((res) => {
